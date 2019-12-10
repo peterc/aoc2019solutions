c6ff5252374e16187641e70a1ae461a9211e46d9
Peter Cooper <git@peterc.org>
2019-12-10 16:27:21 +0000

    Shorter day 8 part 2

--- 8b.rb
+++ 8b.rb
@@ -1,8 +1,6 @@
 puts File.read('8.sif')
       .chars
       .each_slice(25 * 6)      
-      .inject { |a, b| 
-        a.zip(b).map { |a, b| a == '2' ? b : a }
-      }
+      .inject { |a, b| a.zip(b).map { |a, b| a == '2' ? b : a } }
       .map { |c| %w{` #}[c.to_i] }
       .each_slice(25).map(&:join).join("\n")
