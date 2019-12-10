d64dab4311eaac83307c4b8331b09155c76e2b3e
Peter Cooper <git@peterc.org>
2019-12-10 20:20:22 +0000

    Improvements

--- 10b.rb
+++ 10b.rb
@@ -5,46 +5,35 @@ chart = File.readlines(ARGV.first || 'map1').map(&:chomp).map(&:chars)
 
 # Get x, y coördinates of every asteroid
 asteroids = chart.map.with_index { |row, y| row.each_index.select { |x| row[x] == '#' }.map { |x| [x, y] } }.flatten(1)
-results = []
-
-matches = []
 
 # Go over each asteroid and cross reference with every other asteroid
 ax = 20
 ay = 20
 
-asteroids.each do |(bx, by)|
-  # Don't count ourselves
-  next if ax == bx && ay == by
-
+matches = (asteroids - [[ax, ay]]).map do |(bx, by)|
+  # Figure out the smallest cartesian interval between asteroids of this slope
   xdiff = bx - ax
   ydiff = by - ay
-
-  # Figure out the smallest cartesian interval between asteroids of this slope
   dx = xdiff / xdiff.gcd(ydiff)
   dy = ydiff / xdiff.gcd(ydiff)
   visible = true
 
-  # Create a "pseudo" angle of sorts.. to be honest
-  # I ran out of time and can surely optimize this but it works
-  angle = (1 - (1.0 - (bx-ax).to_f/((bx-ax).abs+(ay-by).abs)) * ((ay-by) <=> 0)) % 4
+  # Create a pseudo-angle, spun round so up = 0 (up-1º = 3.999)
+  angle = xdiff.to_f/(xdiff.abs+(-ydiff).abs)
+  angle = -angle + 2 if (-ydiff <=> 0) == -1
+  angle %= 4
 
   # Check spots in between
   tx = ax + dx
   ty = ay + dy
   until (tx == bx && ty == by) do
-    visible = false if chart[ty][tx] != '.'
+    visible = false && break if chart[ty][tx] != '.'
     tx += dx
     ty += dy
   end
 
-  if visible
-    matches << [bx, by, angle]
-  end
-  
-  visible
+  [bx, by, angle] if visible
 end
 
-matches = matches.sort_by { |m| m[2] }
-
-p matches[199]
+matched = matches.compact.sort_by { |m| m[2] }[199]
+puts "#{matched[0] * 100 + matched[1]} should be 317"
