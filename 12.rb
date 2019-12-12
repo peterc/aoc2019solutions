79de978221bdb221e10b4bee16c8202b66b19cf6
Peter Cooper <git@peterc.org>
2019-12-12 16:56:09 +0000

    As nice as 12 is gunna get

--- 12.rb
+++ 12.rb
@@ -1,32 +1,13 @@
-class Position < Struct.new(:x, :y, :z)
-  def total
-    x.abs + y.abs + z.abs
-  end
-end
-
-class Velocity < Struct.new(:x, :y, :z)
-  def total
-    x.abs + y.abs + z.abs
-  end
-end
-
 class Body
-  attr_accessor :position, :velocity
-
-  def x; position.x; end
-  def y; position.y; end
-  def z; position.z; end
-  def vx; velocity.x; end
-  def vy; velocity.y; end
-  def vz; velocity.z; end
-
+  attr_accessor :p, :v
+  
   def initialize(x, y, z)
-    @position = Position.new(x, y, z)
-    @velocity = Velocity.new(0, 0, 0)
+    @p = Struct.new(:x, :y, :z).new(x, y, z)
+    @v = Struct.new(:x, :y, :z).new(0, 0, 0)
   end
 
   def energy
-    @position.total * @velocity.total
+    @p.sum(&:abs) * @v.sum(&:abs)
   end
 end
 
@@ -38,34 +19,21 @@ class System
   def tick
     # Calculate velocities
     @bodies.permutation(2).each do |(b1, b2)|
-      b1.velocity.x += b2.x <=> b1.x
-      b1.velocity.y += b2.y <=> b1.y
-      b1.velocity.z += b2.z <=> b1.z
+      b1.v.x += b2.p.x <=> b1.p.x
+      b1.v.y += b2.p.y <=> b1.p.y
+      b1.v.z += b2.p.z <=> b1.p.z
     end
 
     # Update positions
     @bodies.each do |b|
-      b.position.x += b.vx
-      b.position.y += b.vy
-      b.position.z += b.vz
+      b.p.x += b.v.x
+      b.p.y += b.v.y
+      b.p.z += b.v.z
     end
   end
 
-  def vectors
-    @bodies.map { |body| [x: body.x, y: body.y, z: body.z, vx: body.vx, vy: body.vy, vz: body.vz ] }
-  end
-
-  def velocities
-    @bodies.map { |body| [vx: body.vx, vy: body.vy, vz: body.vz ] }
-  end
-
-  BIT_WIDTH = 12
-  def positions
-    @bodies.map { |body| [body.x + 2000, body.y + 2000, body.z + 2000] }
-  end
-
-  def to_s
-    vectors.map(&:inspect).join("\n")
+  def positions_for(dimension)
+    @bodies.map { |body| body.p[dimension] }
   end
 
   def energy
@@ -83,43 +51,33 @@ moons = System.new([
 
 # PART 1
 # 1000.times { moons.tick }
-# puts moons
-# exit
 # puts moons.energy
+# exit
 
 # PART 2
 
-res = []
+puts "Lowest common multiple is.."
 
-(0..2).each do |facet|
+puts (0..2).map { |dimension|
   hs = {}
-  stage = :step1
-  step1 = 0
+  t = 0
 
   10000000.times do |i|
+    # Run a tick of the simulation
     moons.tick
-    ps = moons.positions
 
-    amt = (ps[0][facet] << 48) + (ps[1][facet] << 32) + (ps[2][facet] << 16) + ps[3][facet]
+    # Turn all the coordinates in a single dimension into a single number
+    amt = moons.positions_for(dimension).pack("ssss")
 
-    str = amt.to_s
-
-    if hs[str]
-      if i - hs[str] == 1 && stage == :step1
-        puts "facet #{facet} start: #{i}"
-        step1 = i
-        stage = :step2
-      elsif i - hs[str] == 1 && stage == :step2
-        puts "facet #{facet} end  : #{i} ... so #{i - step1} cycle length"
-        res << (i - step1)
-        stage = :step1
-        break
-      end
+    # No match? Keep a record for now
+    if !hs[amt]
+      hs[amt] = i
+    elsif i - hs[amt] == 1
+      # Is the match directly following an exact same set of coordinates?
+      # If so, it's a point where the direction is inverting!      
+      ord = t
+      t = i - ord
+      break t if ord > 0
     end
-
-    hs[str] = i
   end
-
-end
-
-puts res.reduce(1, :lcm) * 2
+}.reduce(1, :lcm) * 2
